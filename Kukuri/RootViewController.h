d045c268735b7055af8848c25fbc04181f1d998f
giginet <giginet.net@gmail.com>
2011-10-19 09:15:56 +0900

    Initial Commit

---
+++ Kukuri/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Kukuri
+//
+//  Created by giginet on 11/10/19.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Kukuri/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Kukuri
+//
+//  Created by giginet on 11/10/19.
+//  Copyright __MyCompanyName__ 2011年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
